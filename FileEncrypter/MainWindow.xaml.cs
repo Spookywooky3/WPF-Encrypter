f19f671d62bf031af63c82fffaf6b2fe185002ce
Spookywooky3 <33356007+Spookywooky3@users.noreply.github.com>
2019-12-03 10:14:44 +0800

    Added reference to System.Runtime.InteropServices

--- FileEncrypter/MainWindow.xaml.cs
+++ FileEncrypter/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Security.Cryptography;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace FileEncrypter
 {
